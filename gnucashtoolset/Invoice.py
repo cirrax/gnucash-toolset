caa4c90e1b4e98e6c53374adf5a69b32231ef85f
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2015-02-27 00:25:01 +0100

    add Invoice module to export Invoice from gnucash
    Initial work. Features missing:
    
    - Tax calculation in Subtotal
    - Discount calculation in Subtotal
    - Currency class
    - Probably some more !

---
+++ gnucashtoolset/Invoice.py
@@ -0,0 +1,172 @@
+#
+#    Copyright (C) 2014  Cirrax GmbH  http://www.cirrax.com
+#    Benedikt Trefzer <benedikt.trefzer@cirrax.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   
+
+import datetime
+from gnucash.gnucash_business import  Invoice as gcInvoice
+from gnucash.gnucash_business import  Entry   as gcEntry
+
+import Query
+from gnucash import GncNumeric
+import json
+import JsonImport
+
+DENOM_QUANTITY=1000
+DENOM_PRICE=1000
+GC_ENC='utf-8'
+
+def fromjson_filter(x, date_format):
+    try:
+        return json.loads(x,object_hook= lambda y: JsonImport.json_input(obj=y,date_format=date_format))
+    except ValueError:
+        return x
+
+def date_filter(x, y):
+    try:
+        return x.strftime(y)
+    except AttributeError:
+        return x
+
+class BaseAccounting():
+    def __init__(self, obj=None):
+        self.obj=obj
+
+class Invoice(BaseAccounting):
+
+    def to_dict(self):
+        assign_customer = {
+            'Customer' : lambda x: x,
+            'Job'      : lambda x: x.GetOwner(),
+        }
+	if self.obj==None:
+	    return {}
+
+        return {
+            'ID'            : self.obj.GetID().decode(GC_ENC),
+            'DateDue'       : self.obj.GetDateDue(),
+            'DateOpened'    : self.obj.GetDateOpened(),
+            'DatePosted'    : self.obj.GetDatePosted(),
+
+            'Notes'         : (self.obj.GetNotes() or '').decode(GC_ENC),
+            'Total'         : self.obj.GetTotal().to_double(),
+            'TotalSubtotal' : self.obj.GetTotalSubtotal().to_double(),
+            'TotalTax'      : self.obj.GetTotalTax().to_double(),
+
+            'TypeString'    : self.obj.GetTypeString().decode(GC_ENC),
+
+            'Customer'      : Customer(obj=assign_customer[type(self.obj.GetOwner()).__name__](self.obj.GetOwner())).to_dict(),
+
+            'entries'       : [ Entry(obj=entry).to_dict() for entry in self.obj.GetEntries()],
+  
+            'currency'      : self.obj.GetCurrency().get_mnemonic().decode(GC_ENC),
+            'terms'         : Term(obj=self.obj.GetTerms()).to_dict(),
+        }
+
+class Tax(BaseAccounting):
+    def to_dict(self):
+	if self.obj==None:
+	    return {}
+        return {
+            'TaxName': self.obj.GetName().decode(GC_ENC),
+        }
+
+class Account(BaseAccounting):
+    def to_dict(self):
+	if self.obj==None:
+	    return {}
+        return {
+            'Name' : self.obj.GetName().decode(GC_ENC),
+            'Code' : self.obj.GetCode().decode(GC_ENC),
+            'Notes': (self.obj.GetNotes() or '').decode(GC_ENC),
+        }
+
+class Entry(BaseAccounting):
+
+    def to_dict(self):
+	if self.obj==None:
+	    return {}
+        return {
+            'Date'        : self.obj.GetDate(),
+            'DateEntered' : self.obj.GetDateEntered(),
+            'Action'      : self.obj.GetAction().decode(GC_ENC),
+            'Description' : self.obj.GetDescription().decode(GC_ENC),
+            'Notes'       : self.obj.GetNotes().decode(GC_ENC),
+            'Quantity'    : self.obj.GetQuantity().to_double(),
+            'Price'       : self.obj.GetInvPrice().to_double(),
+            'TaxIncluded' : self.obj.GetInvTaxIncluded(),
+            'Taxable'     : self.obj.GetInvTaxable(),
+            'TaxTable'    : Tax(obj=self.obj.GetInvTaxTable()).to_dict(),
+            'AccountTable': Account(obj=self.obj.GetInvAccount()).to_dict(),
+            'CalcDiscount': self._calc_discount(),
+            'CalcSubtotal': self._calc_amount('subtotal'),
+        }
+
+    def _calc_discount(self):
+        # dummy function, calculates amount for discount, currently 0 returned !
+        return 0
+
+    def _calc_amount(self, typ):
+        # TODO: This function should include Tax and discount calculation.
+        amount={}
+        quantity=self.obj.GetQuantity().to_double()
+        price=self.obj.GetInvPrice().to_double()
+        amount['subtotal']=quantity * price
+        amount['total']=quantity * price
+
+        return amount[typ]
+
+class Customer(BaseAccounting):
+
+    def to_dict(self):
+	if self.obj==None:
+	     return {}
+        return {
+             'ID'       : self.obj.GetID().decode(GC_ENC),
+             'Name'     : self.obj.GetName().decode(GC_ENC),
+             'Notes'    : self.obj.GetNotes().decode(GC_ENC),
+             'currency' : self.obj.GetCurrency().get_unique_name().decode(GC_ENC),
+         
+             'Addr'     : Address(obj=self.obj.GetAddr()).to_dict(),
+             'ShipAddr' : Address(obj=self.obj.GetShipAddr()).to_dict(),
+        }
+
+class Address(BaseAccounting):
+
+    def to_dict(self):
+	if self.obj==None:
+	    return {}
+        return {
+            'Name'  : self.obj.GetName().decode(GC_ENC),
+            'Addr1' : self.obj.GetAddr1().decode(GC_ENC),
+            'Addr2' : self.obj.GetAddr2().decode(GC_ENC),
+            'Addr3' : self.obj.GetAddr3().decode(GC_ENC),
+            'Addr4' : self.obj.GetAddr4().decode(GC_ENC),
+            'Email' : self.obj.GetEmail().decode(GC_ENC),
+            'Fax'   : self.obj.GetFax().decode(GC_ENC),
+            'Phone' : self.obj.GetPhone().decode(GC_ENC),
+        }
+
+class Term(BaseAccounting):
+    def to_dict(self):
+        if self.obj==None:
+	    return {}
+
+        return {
+            'Name'        : self.obj.GetName().decode(GC_ENC),
+            'Description' : self.obj.GetDescription().decode(GC_ENC),
+            'DueDays'     : self.obj.GetDueDays(),
+        }
